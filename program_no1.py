7bd65d20d8bc5cb89b05bdf705d7dbc7b7cb37a6
LAPTOP-7O8QBV3P\lenovo <elijahmaetardiente@iskolarngbayan.pup.edu.ph>
2021-11-04 14:59:00 +0800

    first commit for program no.1

---
+++ program_no1.py
@@ -0,0 +1,6 @@
+name = input("Please enter you name: ")
+age = input("Please enter your age in years: ")
+address = input("Please enter your address: ")
+
+print("\nHi, my name is " + name + ". I am " + age +
+                " years old and I live in " + address + ".")
