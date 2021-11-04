0c190e377aa89e1f6d773864c6d029b31be70026
LAPTOP-7O8QBV3P\lenovo <elijahmaetardiente@iskolarngbayan.pup.edu.ph>
2021-11-04 16:43:12 +0800

    added restrictions for inputs

--- program_no3.py
+++ program_no3.py
@@ -1,19 +1,33 @@
-def get_money():
-    money_input = float(
-        input("Please enter the amount of money you have: "))
-    return money_input
+try:
 
-def get_apple():
-    apple_input = float(
-        input("Please enter the price of an apple that you would like to buy: "))
-    return apple_input
+    def get_money():
+        money_input = float(
+            input("Please enter the amount of money you have: "))
+        return money_input
 
-def final_output(moneyF, appleF):
-    max_apple = int(moneyF//appleF)
-    change = moneyF % appleF
+    def get_apple():
+        apple_input = float(
+            input("Please enter the price of an apple that you would like to buy: "))
+        if(apple_input == 0.0):
+            print("Sorry, we don't have apples that cost 0.00 pesos.")
+            quit()
+        else:
+            return apple_input
 
-    print(f"You can buy {max_apple} apples and your change is {change:.2f} pesos.")
+    def final_output(moneyF, appleF):
+        max_apple = int(moneyF//appleF)
+        change = moneyF % appleF
 
-money = get_money()
-apple = get_apple()
-final_output(money, apple)
+        print(
+            f"You can buy {max_apple} apples and your change is {change:.2f} pesos.")
+
+    money = get_money()
+    apple = get_apple()
+
+    if(money < 0 or apple < 0):
+        print("Error. Less than 0 inputs are invalid.")
+    else:
+        final_output(money, apple)
+
+except ValueError:
+    print("Invalid input. Please input a number.")
