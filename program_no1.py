37e697ffac1cf7f91803db93a72d17a0058097b4
LAPTOP-7O8QBV3P\lenovo <elijahmaetardiente@iskolarngbayan.pup.edu.ph>
2021-11-04 15:14:46 +0800

    codes for getting of inputs grouped into functions

--- program_no1.py
+++ program_no1.py
@@ -1,6 +1,19 @@
-name = input("Please enter you name: ")
-age = input("Please enter your age in years: ")
-address = input("Please enter your address: ")
+def get_name():
+    name = input("Please enter you name: ")
+    return name
+
+def get_age(): 
+    age = input("Please enter your age in years: ")
+    return age
+
+def get_address():
+    address = input("Please enter your address: ")
+    return address
+
+name = get_name()
+age = get_age()
+address = get_address()
 
 print("\nHi, my name is " + name + ". I am " + age +
                 " years old and I live in " + address + ".")
+
