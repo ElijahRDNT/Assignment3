ce7fa22493577ab63c73a0757e1d7efe98db12cc
LAPTOP-7O8QBV3P\lenovo <elijahmaetardiente@iskolarngbayan.pup.edu.ph>
2021-11-04 16:14:41 +0800

    added restrictions for inputs

--- program_no2.py
+++ program_no2.py
@@ -1,19 +1,29 @@
 apple_price = 20
 orange_price = 25
 
-def get_apples():
-    apple_input = int(input("Please enter the number of apples you want to buy: "))
-    return apple_input
+try:
+    def get_apples():
+        apple_input = int(
+            input("Please enter the number of apples you want to buy: "))
+        return apple_input
 
-def get_oranges():    
-    orange_input = int(input("Please enter the number of oranges you want to buy: "))
-    return orange_input
+    def get_oranges():
+        orange_input = int(
+            input("Please enter the number of oranges you want to buy: "))
+        return orange_input
 
-def finalOutput(applesF, orangesF):
-    amount = ((applesF*apple_price) +
+    def finalOutput(applesF, orangesF):
+        amount = ((applesF*apple_price) +
                   (orangesF*orange_price))
-    print("The total amount is Php" + str(amount) + ".00.")
+        print("The total amount is Php" + str(amount) + ".00.")
 
-apples = int(get_apples())
-oranges = int(get_oranges())
-finalOutput(apples, oranges)
+    apples = int(get_apples())
+    oranges = int(get_oranges())
+
+    if(apples < 0 or oranges < 0):
+        print("Invalid order. Less than 0 inputs are not accepted.")
+    else:
+        finalOutput(apples, oranges)
+
+except ValueError:
+    print("Error. Please enter whole numbers.")
